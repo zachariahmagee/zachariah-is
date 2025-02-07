4bf9cd67ee0d180a69bab5b15aab2a9554a499af
Zachariah Magee <103608503+zachariahmagee@users.noreply.github.com>
2025-02-07 14:13:03 -0800

    added contact page but need to build the email form

---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+
+
+
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Contact</h1>
+      <p>Shoot me an email at zachariahmagee@gmail.com</p>
+
+    </section>
+  )
+}
+
