ee96cd0604542c1e5c073ff1aa482d730d3f7041
Zachariah Magee <103608503+zachariahmagee@users.noreply.github.com>
2025-05-04 14:25:05 -0700

    changed the page titles

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,15 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Zachariah is...',
+    template: '%s | Zachariah is...',
   },
   description: 'This is my portfolio.',
   openGraph: {
-    title: 'My Portfolio',
-    description: 'This is my portfolio.',
+    title: 'Zachariah is...',
+    description: 'Zachariah Magee\'s blog and portfolio.',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'Zachariah is...',
     locale: 'en_US',
     type: 'website',
   },
